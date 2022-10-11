4a06e02c2904eeb4cfd2bc412e5601fc05fa2410
Florian Fusseder <fussederflorian@gmail.com>
2022-10-11 15:18:48 +0200

    Webscraber

--- components.py
+++ components.py
@@ -1,28 +1,37 @@
+from typing import Type, Optional, Dict
+
 import numpy as np
 
 from pieces import p_arrays, b_array
 
 
-class Location:
+def toASCII(shape):
+    if shape.any():
+        return str(shape).replace(' ', '').replace('[', '').replace(']', '').replace('0', ' ').replace('1', '█')
 
+
+class Location:
     def __init__(self, x: int, y: int) -> None:
         self.x = x
         self.y = y
 
 
 class Piece:
-    def __init__(self, id_, button_cost, time_cost, production, shape) -> None:
+    def __init__(self, id_, button_cost, time_cost, button_income, shape=None) -> None:
         self.id_ = id_
         self.button_cost = button_cost
         self.time_cost = time_cost
-        self.production = production
+        self.button_income = button_income
         self.shape = np.array(shape)
         self.size = np.count_nonzero(self.shape)
 
     def __str__(self) -> str:
         return f"Id: {self.id_}, ButtonCost: {self.button_cost}, TimeCost: {self.time_cost}, " \
-               f"Production: {self.production}, Size: {self.size}" \
-               f"\n {str(self.shape).replace('[', '').replace(']', '')}"
+               f"Production: {self.button_income}, Size: {self.size}"
+
+    def get_button_rate(self, remaining_income_phases) -> float:
+        # Button-rate = [(size x 2) + (buttons x remaining income phases) - button cost] / time cost
+        return ((self.size * 2) + (self.button_income * remaining_income_phases) - self.button_cost) / self.time_cost
 
     def show(self):
         copy = np.copy(self.shape)
@@ -33,16 +42,14 @@ class Piece:
         convert.resize(s * 40 for s in convert.size).show()
 
 
-class Pieces:
+class Patches:
 
-    def __init__(self) -> None:
+    def __init__(self, patches, token_position) -> None:
         self.collection = []
+        self.token_position = token_position
 
-        for i, p in p_arrays.items():
-            self.collection.append(Piece(i, p[0], p[1], p[2], p[3]))
-
-    def remove_piece(self, piece):
-        pass
+        for id_, patch in patches.items():
+            self.collection.append(Piece(id_, patch['button-cost'], patch['time-cost'], patch['income']))
 
     def __len__(self):
         return len(self.collection)
@@ -54,54 +61,10 @@ class Pieces:
         return "\n".join([str(p) for p in self.collection])
 
 
-class Player:
-    class Board:
-
-        def __init__(self) -> None:
-            self.board = np.array(b_array)
-
-    def __init__(self, player_number: int, player_name: str):
-        self.player_name = player_name
-        self.player_number = player_number
-        self.player_token = TimeTrack.TimeTrackToken(player_number)
-        self.button_production = 0
-        self.button_count = 5
-
-    def __str__(self) -> str:
-        return f"{self.player_number}"
-
-    def status(self) -> str:
-        return f"Player {self.player_number} ({self.player_name}): Buttons: {self.button_count}, Token: ({self.player_token})"
-
-    def calculate_turn(self, pieces: Pieces) -> (Piece, Location):
-        pass
-
-    def calculate_bonus_turn(self):
-        pass
-
-    def put_piece(self, piece, location):
-        pass
-
-
 class TimeTrack:
     _goal_id = 53
     _income_locations = [5, 11, 17, 23, 29, 35, 41, 47, _goal_id]
     _special_spot_locations = [26, 32, 38, 44, 50]
-
-    class TimeTrackToken:
-
-        def __init__(self, player_number: int) -> None:
-            self.distance = 0
-            self.on_top = player_number == 1
-
-        def __gt__(self, other):
-            return self.distance > other.distance or self.distance == other.distance and self.on_top
-
-        def __lt__(self, other):
-            return self.distance < other.distance or self.distance == other.distance and not self.on_top
-
-        def __str__(self) -> str:
-            return f"Distance: {self.distance} Top: {self.on_top}"
 
     class TimeTrackField:
         def __init__(self, id_: int) -> None:
@@ -110,18 +73,42 @@ class TimeTrack:
             self.trigger_special_spot_income = id_ in TimeTrack._special_spot_locations
             self.id_ = id_
 
-        def visit(self, player: Player):
-            pass
-
-    def __init__(self, p1: Player, p2: Player) -> None:
+    def __init__(self) -> None:
         self.track = []
-        for i in range(54):
+        for i in range(53, -1, -1):
             self.track.append(TimeTrack.TimeTrackField(i))
-        self.p1_token = p1.player_token
-        self.p2_token = p2.player_token
 
-    def done(self) -> bool:
-        return self.p1_token.distance == self._goal_id and self.p2_token == self._goal_id
+    def get_remaining_income_phases(self, player) -> int:
+        return player.time_count
 
-    def advance_player(self, active_player: Player, piece: Piece) -> Piece:
-        return Piece()
+
+class Player:
+    class Board:
+
+        def __init__(self) -> None:
+            self.board = np.array(b_array)
+
+        def __str__(self):
+            return toASCII(self.board)
+
+    def __init__(self, player_number: int, player_data: Dict):
+        self.player_number = player_number
+        self.player_name = player_data["name"]
+        self.time_count = player_data["time_counter"]
+        self.button_production = player_data["income_counter"]
+        self.button_count = player_data["button_counter"]
+        self.color_code = player_data["color_code"]
+        self.player_turn = player_data["current_player"]
+        self.player_track = TimeTrack()
+        self.board = Player.Board()
+
+    def __str__(self) -> str:
+        return f"{self.player_name}"
+
+    def status(self) -> str:
+        return f"Player {self.player_number} {self.player_name}: Buttons: {self.button_count}, " \
+               f"ButtonProduction: {self.button_production}, Time: {self.time_count}"
+
+    def calculate_turn(self, patches: Patches) -> (Piece, Location):
+        remaining_income_phases = self.player_track.get_remaining_income_phases(self)
+        return patches[0], Location(0, 0)
--- pw.py
+++ pw.py
@@ -1,36 +1,125 @@
-from PIL import Image
+from typing import Dict
 
-from components import Pieces, Player, TimeTrack
-import numpy as np
+from components import Patches, Player, TimeTrack
 
 
-def init_game() -> (TimeTrack, Pieces, Player, Player):
+def read_game():
+    print("Read data...")
+    from selenium.webdriver import Firefox
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.firefox.options import Options
+    from selenium.webdriver.support import expected_conditions
+    from selenium.webdriver.support.wait import WebDriverWait
+    from bs4 import BeautifulSoup
+
+    options = Options()
+    options.add_argument('--headless')
+    driver = Firefox(options=options)
+    driver.get('https://boardgamearena.com/6/patchwork?table=307392525')
+
+    ids = [
+        "patch_1",
+        "patch_2",
+        "patch_3",
+        "patch_4",
+        "patch_5",
+        "patch_6",
+        "patch_7",
+        "patch_8",
+        "patch_9",
+        "patch_10",
+        "patch_11",
+        "patch_12",
+        "patch_13",
+        "patch_14",
+        "patch_15",
+        "patch_16",
+        "patch_17",
+        "patch_18",
+        "patch_19",
+        "patch_20",
+        "patch_21",
+        "patch_22",
+        "patch_23",
+        "patch_24",
+        "patch_25",
+        "patch_26",
+        "patch_27",
+        "patch_28",
+        "patch_29",
+        "patch_30",
+        "patch_31",
+        "patch_32",
+        "patch_33"
+    ]
+
+    for id_ in ids:
+        WebDriverWait(driver, 30).until(expected_conditions.presence_of_all_elements_located((By.ID, id_)))
+
+    element = driver.find_element(By.ID, "overall-content")
+    html = element.get_attribute('outerHTML')
+
+    soup = BeautifulSoup(html, "html.parser")
+    patches_html = soup.select(".patch.flipper")
+
+    patches = dict()
+    token = soup.find("div", class_="token_neutral")
+
+    for elem in patches_html:
+        if elem['id'].startswith("patch_"):
+            title_: [str] = elem['title'].split()
+
+            patches[elem['id']] = {
+                'data-order': elem['data-order'],
+                'button-cost': title_[2],
+                'time-cost': title_[5],
+                'income': title_[-1]
+            }
+
+    player_html = soup.find('div', id="player_boards")
+    player = {}
+
+    for player_board in player_html.findChildren("div", class_="player-board"):
+        player_id = int(player_board["id"].split("_")[-1])
+        current_player = player_html.findChild("div", id=f"avatar_active_wrap_{player_id}")['style']
+        player_ref = player_board.findChild("a")
+        player_name = player_ref.text
+        player_color_code = player_ref["style"].split("#")[-1]
+
+        time_counter = player_board.findChild("div", class_="time_counter mini_counter").text
+        button_counter = player_board.findChild("div", class_="buttons_counter mini_counter").text
+        income_counter = player_board.findChild("div", class_="income_counter subtext mini_counter").text
+
+        player[player_id] = {
+            "current_player": current_player == 'display: block;',
+            "name": player_name,
+            "time_counter": time_counter,
+            "button_counter": button_counter,
+            "income_counter": income_counter,
+            "color_code": player_color_code,
+        }
+
+    driver.quit()
+    return patches, token['data-order'], player
+
+
+def init_game(patches: Dict, token_position: int, players: Dict) -> (TimeTrack, Patches, Player, Player):
     print("Init data structure...")
-    pieces = Pieces()
-    p1 = Player(1, "Sonja")
-    p2 = Player(2, "Florian")
-    track = TimeTrack(p1, p2)
-    return track, pieces, p1, p2
+    pieces = Patches(patches, token_position)
+    popitem = players.popitem()
+    p1 = Player(popitem[0], popitem[1])
+    popitem = players.popitem()
+    p2 = Player(popitem[0], popitem[1])
+    return pieces, p1, p2
 
 
 def go_play():
-    track, pieces, p1, p2 = init_game()
-    print(pieces)
-    turn_counter = 0
-    while not track.done():
-        active_player: Player = p1 if p1.player_token > p2.player_token else p2
-        print(f"Turn {turn_counter}: Player {active_player}'s turn")
-        piece, location = active_player.calculate_turn(pieces)
-        pieces.remove_piece(piece)
-        active_player.put_piece(piece, location)
-        special_patch = track.advance_player(active_player, piece)
-        if special_patch:
-            active_player.calculate_bonus_turn()
-        turn_counter += 1
+    patches, token_position, players = read_game()
+    pieces, p1, p2 = init_game(patches, int(token_position), players)
+    active_player: Player = p1 if p1.player_turn else p2
+    print(f"{active_player.player_name}'s turn ({active_player.status})")
+    active_player.calculate_turn(pieces)
 
 
 if __name__ == "__main__":
-    pieces = Pieces()
-    print(len(pieces))
-    pieces[1].show()
-    print(pieces)
+    go_play()
